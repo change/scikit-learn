42f9c44a47e4d71526742ea23c11fa760ab62194
Fernand Pajot <fernand@change.org>
2013-03-07 15:48:00 -0800

    added sklearn.datasets.svmlight-loader, a faster implementation of load_svmlight_file 2

---
+++ sklearn/datasets/svmlight-loader/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Extension
+import numpy as np
+
+#!/usr/bin/env python
+
+import numpy
+import os
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('svmlight-loader', parent_package, top_path)
+    config.add_extension('_svmlight_loader',
+            include_dirs=[numpy.get_include()],
+            sources = ['_svmlight_loader.cpp'],
+            extra_compile_args=['-O3'])
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
